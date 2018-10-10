15dfe6da1451de4a612b365017a6520c16a7f302
DESKTOP-S521LBT\corys <corysivek@gmail.com>
2018-10-10 15:54:53 -0600

    Changing the Model
    Changing the model

--- lib/model.dart
+++ lib/model.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
 
-class CardModel {
+enum ContentAPI {
+  Cory, Vimeo, Slack, Sketchfab
+}
+
+class ModelManager {
+  Map<String, ArtistData> artists;
+  Map<String, PostData> posts;
+  Map<String, AssetData> assets;
+
+  ArtistData GetArtist(String artist_id) {
+    if (artists.containsKey(artist_id)) {
+      return artists[artist_id];
+    } else {
+      ArtistData artist = loadArtist(artist_id);
+      if (artist) {
+        artists[artist_id] = artist;
+      }
+    }
+  }  
+}
+
+class ArtistData {
+  String _id;
+  String _name;
+  String _thumbnail;
+}
+
+class PostData {
+  String _id;
+  String _artist_id;
+  String _asset_id;
+  ContentAPI _content_api;
+  String _content;
+  String _department;
+  DateTime timestamp;
+  String _slack_link;
+
+}
+
+class AssetData {
+  String _id;
+  String _name;
+  String _thumbnail;
+  List<String> _posts_ids;
+  List<String> _departments;
   
 }
 
-class PostData extends CardModel {
-  
-}
-
-class AssetData extends CardModel {
-  
-}
-
-class NotificationData extends CardModel {
+class NotificationData {
   String _artist;
   String _department;
   String _asset;
