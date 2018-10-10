69a72b4712df16d1821ed5606f3a34ccb2125f63
htinney <hunter.tinney@gmail.com>
2018-10-10 15:53:16 -0600

    Creating hooks for Kendra to add JSON reading later.

---
+++ lib/json_loader.dart
@@ -0,0 +1,42 @@
+import 'model.dart';
+
+AssetData loadAsset(String assetID) {
+  return null;
+}
+
+PostData loadPost(String postID) {
+  return null;
+}
+
+NotificationData loadNotification(String notificationID) {
+  return null;
+}
+
+ArtistData loadArtist(String artistID) {
+  return null;
+}
+
+
+Map<String, AssetData> loadAssets() {
+  return [
+    new AssetData(),
+    new AssetData(),
+    new AssetData()
+  ];
+}
+
+Map<String, PostData> loadPosts() {
+  return [
+    new PostData(),
+    new PostData(),
+    new PostData()
+  ];
+}
+
+Map<String, NotificationData> loadNotifications() {
+  return [
+    new NotificationData(),
+    new NotificationData(),
+    new NotificationData()
+  ];
+}
