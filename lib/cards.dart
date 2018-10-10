bee88e66192bccc9848521dd0a37b480d36d25de
htinney <hunter.tinney@gmail.com>
2018-10-10 14:32:39 -0600

    Resolved merge conflicts.

--- lib/cards.dart
+++ lib/cards.dart
@@ -26,11 +26,7 @@ class NotificationCardState extends State<NotificationCard> {
   Widget build(BuildContext context) {
     BorderSide blackLine = new BorderSide(
       color: Colors.black54,
-<<<<<<< HEAD
       width: 1.0,
-=======
-      width: 2.0,
->>>>>>> 73fdd6185f8009482c3cd0b4debd06155207f6d7
     );
     Color myColor = n.clicked ? Colors.white : Theme.of(context).bottomAppBarColor;
     return Container(
@@ -40,10 +36,7 @@ class NotificationCardState extends State<NotificationCard> {
         color: myColor,
         border: new Border(
           bottom: blackLine,
-<<<<<<< HEAD
           top: new BorderSide(width:0.0,),
-=======
->>>>>>> 73fdd6185f8009482c3cd0b4debd06155207f6d7
         ),
       ),
 //        color: Colors.white,
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:flutter/material.dart';
 import 'top_bar.dart';
 import 'cards.dart';
 import 'model.dart';
-<<<<<<< HEAD
 import 'main_list_view.dart';
-=======
->>>>>>> 73fdd6185f8009482c3cd0b4debd06155207f6d7
 
 void main() => runApp(new Pipelion());
 
@@ -107,12 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 _topBar,
-<<<<<<< HEAD
                 _mainListView,
-=======
-                NotificationCard(NotificationData(true)),
-                NotificationCard(NotificationData(false)),
->>>>>>> 73fdd6185f8009482c3cd0b4debd06155207f6d7
               ],
             ),
           ),
