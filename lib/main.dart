177f0efd8d8a69d50db4d0211859cda4d13bf250
Kendra Graham <deloris4@gmail.com>
2018-10-09 18:31:21 -0600

    moved the TopBar widget into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'top_bar.dart';
 
 void main() => runApp(new Pipelion());
 
@@ -32,15 +33,6 @@ class Pipelion extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,13 +44,21 @@ class _MyHomePageState extends State<MyHomePage> {
   _MyHomePageState() {
     _showFilters = false;
     _currentPage = Page.posts;
-    _topBar = new TopBar();
+    _searchText = "default search text";
+    _topBar = new TopBar(_updateSearch);
   }
 
   bool _showFilters;
   Page _currentPage;
+  String _searchText;
 
   TopBar _topBar;
+
+  void _updateSearch(String searchText) {
+    setState(() {
+      _searchText = searchText;
+    });
+  }
 
   void _onHomeClicked() {
     setState(() {
@@ -84,12 +84,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     EdgeInsets menuButtonsPadding = new EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0);
 
     return new Scaffold(
@@ -101,10 +95,10 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 _topBar,
-//            new Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
+            new Text(
+              _searchText,
+              style: Theme.of(context).textTheme.display1,
+            ),
               ],
             ),
           ),
@@ -136,106 +130,4 @@ class _MyHomePageState extends State<MyHomePage> {
 
     );
   }
-}
-
-class TopBar extends StatefulWidget {
-
-  void setCurrentPage(Page page) {
-    myState._setCurrentPage(page);
-  }
-
-  TopBarState myState;
-
-  @override
-  State<StatefulWidget> createState() {
-    myState = new TopBarState();
-    return myState;
-  }
-}
-
-class TopBarState extends State<TopBar> {
-
-  TopBarState() {
-    currentPage = Page.posts;
-  }
-
-  void _setCurrentPage(Page page) {
-    setState(() {
-      currentPage = page;
-    });
-  }
-
-  Page currentPage;
-  String searchText = "";
-
-  void _onSearch() {}
-
-  @override
-  Widget build(BuildContext context) {
-    if (currentPage == Page.notifications) {
-      return Container(
-//        margin: const EdgeInsets.all(15.0),
-//        padding: const EdgeInsets.all(3.0),
-          decoration: new BoxDecoration(
-            color: Colors.white,
-            border: new Border(
-              bottom: new BorderSide(
-                color: Colors.black,
-                width: 1.0,
-              )
-            ),
-          ),
-//        color: Colors.white,
-        height: 70.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Notifications",
-              style: new TextStyle(fontSize: 16.0),
-            ),
-          ]
-        ),
-      );
-    }
-    else {
-      return Container(
-        padding: EdgeInsets.all(8.0),
-        decoration: new BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black54,
-              blurRadius: 3.0, // has the effect of softening the shadow
-              spreadRadius: -8.0, // has the effect of extending the shadow
-              offset: new Offset(2.0, 2.0)
-            )
-          ]
-        ),
-        child: Center(
-          child: Container(
-            decoration: new BoxDecoration(
-              borderRadius: new BorderRadius.all(new Radius.circular(3.0)),
-              color: Colors.white,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-                  child: Icon(Icons.search),
-                ),
-                Text(pageSearchTerms[currentPage]),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 2.0, 0.0),
-                  child: IconButton(
-                    icon: Icon(Icons.filter_list), onPressed: _onSearch),
-                ),
-              ]
-            ),
-          ),
-        ),
-      );
-    }
-  }
-
 }
